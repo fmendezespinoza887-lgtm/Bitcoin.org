731bed89c807920b974c3d4f2dc0421bb4cc9bf7
fmendezespinoza887-lgtm <fmendezespinoza887@gmail.com>
2026-07-10 20:33:00 -0600

    Create 80.js

---
+++ 80.js
@@ -0,0 +1,229 @@
+Skip to content
+Bitcoin.org
+Repository navigation
+Code
+Issues
+109
+ (109)
+Bitcoin.org Website
+
+bitcoin.org/
+License
+ Unknown, Unknown licenses found
+Code of conduct
+ Code of conduct
+Contributing
+ Contributing
+ 1.8k stars
+ 2.1k forks
+ 254 watching
+ 34 Branches
+ 0 Tags
+ Activity
+ Custom properties
+Public repository
+bitcoin-dot-org/Bitcoin.org
+Name	
+devdavidejesus
+devdavidejesus
+2 hours ago
+.tx
+8 years ago
+_alerts
+7 years ago
+_build
+7 years ago
+_contrib
+last year
+_includes
+2 weeks ago
+_layouts
+2 years ago
+_platforms
+7 years ago
+_plugins
+5 years ago
+_posts
+3 weeks ago
+_releases
+12 hours ago
+_sass
+2 days ago
+_templates
+yesterday
+_translations
+yesterday
+_wallets
+3 weeks ago
+css
+6 years ago
+de/bitcoin-core
+11 years ago
+docs
+2 weeks ago
+en
+3 hours ago
+es/bitcoin-core
+11 years ago
+font
+8 years ago
+img
+2 days ago
+it/bitcoin-core
+8 years ago
+js
+last year
+json
+last year
+quality-assurance/schemas
+2 years ago
+ru/bitcoin-core
+11 years ago
+zh_CN/bitcoin-core
+11 years ago
+zh_TW/bitcoin-core
+11 years ago
+.gitattributes
+11 years ago
+.gitignore
+6 years ago
+.jshintrc
+8 years ago
+.mailmap
+8 years ago
+.ruby-version
+6 years ago
+.travis.yml
+6 years ago
+404.html
+11 years ago
+CODE_OF_CONDUCT.md
+last month
+CONTRIBUTING.md
+last month
+COPYING
+12 years ago
+Gemfile
+6 years ago
+Gemfile.lock
+5 years ago
+IL-2021-000008-Wright-v-Cobra.pdf
+5 years ago
+LICENSE
+5 months ago
+Makefile
+5 years ago
+README.md
+3 weeks ago
+_config.yml
+3 weeks ago
+_events.yml
+4 years ago
+andreas_schildbach.asc
+14 years ago
+favicon.ico
+13 years ago
+favicon.png
+13 years ago
+gmaxwell.asc
+13 years ago
+index.html
+8 years ago
+jonasschnelli.asc
+11 years ago
+laanwj-releases.asc
+7 years ago
+laanwj.asc
+7 years ago
+legacy-releases.asc
+8 years ago
+pieterwuille.asc
+11 years ago
+robots.txt
+11 years ago
+satoshinakamoto.asc
+13 years ago
+willbinns.asc
+10 years ago
+Repository files navigation
+README
+Welcome to Bitcoin.org's Codebase
+Current Build Status: Build Status
+
+Live site: Bitcoin.org
+
+Report problems or help improve the site by opening a new issue or pull request.
+
+Earn Bitcoin for Contributing
+Open issues labeled with "Bounty" have bounties on them. Viewing the issue will reveal the value of the bounty. Submit a pull request resolving the issue along with an accompanying note or comment containing a bitcoin address and automatically receive a payment in the amount of the bounty if it gets merged.
+
+How to Participate
+The following quick guides will help you get started:
+
+Becoming a Contributor
+Working with GitHub
+Setting Up Your Environment
+Improving Developer Documentation
+Assisting with Translations
+Adding Exchanges
+Managing Wallets
+Adding Events, Release Notes and Alerts
+Adding Blog Posts
+Miscellaneous / Other
+Code of Conduct
+Participation in this project is subject to a Code of Conduct.
+
+Releases
+No releases published
+Packages
+No packages published
+Contributors
+377
+@wbnns
+@ghost1542
+@Cobra-Bitcoin
+@harding
+@alexchorman
+@kuzzmi
+@devdavidejesus
+@laanwj
+@Mirobit
+@nopara73
+@gavinandresen
+@thebrandonlucas
+@luke-jr
+@crwatkins
++ 363 contributors
+Languages
+HTML
+40.9%
+ 
+SCSS
+30.2%
+ 
+JavaScript
+10.0%
+ 
+Less
+8.3%
+ 
+Ruby
+5.3%
+ 
+Shell
+2.6%
+ 
+Other
+2.7%
+Footer
+© 2026 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Community
+Docs
+Contact
+Manage cookies
+Do not share my personal information
